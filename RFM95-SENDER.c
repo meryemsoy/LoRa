1ce6ccf4c67fe96630292bd9d745283fbd1cf58f
meryemsoy27@gmail.com <meryemsoy27@gmail.com>
2023-08-08 15:18:15 +0300

    LoRa

---
+++ RFM95-SENDER.c
@@ -0,0 +1,37 @@
+#include <LoRa.h>
+#include <SPI.h>
+
+#define SS    5
+#define RESET 14
+#define DIO0  2
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  delay(1000);
+  Serial.println("LoRa Sender");
+  LoRa.setPins(SS,RESET,DIO0);
+  if(!LoRa.begin(868E6)){
+    Serial.println("Starting LoRa Failed");
+    while(1);
+  }
+  else{
+    Serial.println("Starting LoRa Succesfull");
+  }
+
+}
+
+void loop() {
+  Serial.print("Sender packet: ");
+  Serial.println(counter);
+  LoRa.beginPacket();
+  LoRa.print("Hello");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++ ;
+  
+  delay(5000);
+}
